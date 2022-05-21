9a06f10ee0bfedc5123fb126b7593f395beb50f8
unknown <technicalsalma@gmail.com>
2022-05-22 00:25:58 +0600

    navber complete

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import banner from '../../images/banner/banner-2.jpg';
+
+const Banner = () => {
+    return (
+      <div className="w-screen">
+        <img className="w-screen h-96" src={banner} alt="" />
+      </div>
+    );
+};
+
+export default Banner;
